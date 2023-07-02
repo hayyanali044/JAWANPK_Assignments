d5d2818a4d72f84764a19c0cee5fe691ccde2f06
MUHAMMAD HAYYAN ALI <87619716+hayyanali044@users.noreply.github.com>
2023-07-02 06:18:38 +0500

    Update Q1.dart

--- Assignment 3/Q1.dart
+++ Assignment 3/Q1.dart
@@ -1,10 +1,10 @@
 void main() {
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<int> num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  for (int i = 0; i < numbers.length; i++) {
-    int number = numbers[i];
-    if (number % 2 == 0) {
-      print(number);
+  for (int i = 0; i < num_list.length; i++) {
+    int num_list = num_list[i];
+    if (num_list % 2 == 0) {
+      print(num_list);
     }
   }
 }
