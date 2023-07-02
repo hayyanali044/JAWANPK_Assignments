34f1f4d1d3c1ffbd96b008a4a1a79bb7321b37e8
MUHAMMAD HAYYAN ALI <87619716+hayyanali044@users.noreply.github.com>
2023-07-02 12:27:01 +0500

    Add files via upload

---
+++ Assignment 3/Q13.dart
@@ -0,0 +1,11 @@
+void main() {
+  int n = 4;
+
+  for (int i = 1; i <= n; i++) {
+    String pattern = '';
+    for (int j = 1; j <= i; j++) {
+      pattern += i.toString();
+    }
+    print(pattern);
+  }
+}
